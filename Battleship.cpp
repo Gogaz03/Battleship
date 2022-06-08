b702e0b8a1208472b80b00d1c5b11bec7b3e151e
Gogaz03 <106033813+Gogaz03@users.noreply.github.com>
2022-06-08 10:30:15 +0300

    Update Battleship.cpp

--- Battleship.cpp
+++ Battleship.cpp
@@ -1,12 +1,12 @@
 #include "Board.h"
-
+#include "Game.h"
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     srand((unsigned)time(NULL));
 
-    Board a;
+    Game a;
     a.fill_board();
     a.print_board();
     a.game();
