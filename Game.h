cd4e4175075f891ab2882d1d89aec39cf2a8fc87
Gogaz03 <106033813+Gogaz03@users.noreply.github.com>
2022-06-08 10:31:51 +0300

    Create Game.h

---
+++ Game.h
@@ -0,0 +1,16 @@
+#ifndef rr
+#define rr
+#include <iostream>
+#include <locale>
+#include <cstdlib>
+#include <windows.h>
+#include <mmsystem.h>
+#include "Board.h"
+
+class Game : public Board
+{
+public:
+	void game();
+};
+
+#endif 
